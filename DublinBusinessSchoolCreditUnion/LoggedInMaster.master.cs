42ef1d72d094e5ca6af3eb5ab021222b7fb87ce5
robbiegleeson <rob.gleeson@icloud.com>
2015-03-23 15:17:03 +0000

    No real changes

--- DublinBusinessSchoolCreditUnion/LoggedInMaster.master.cs
+++ DublinBusinessSchoolCreditUnion/LoggedInMaster.master.cs
@@ -17,7 +17,7 @@ namespace DublinBusinessSchoolCreditUnion
 
         protected void lblLogout_Click(object sender, EventArgs e)
         {
-            CustomSessionObject.Current.SessionUsername = null;
+            Session.Remove("Username");
             Server.Transfer("Default.aspx");
         }
     }
