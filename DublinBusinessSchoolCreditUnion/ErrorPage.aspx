ffd676dc05f5cb2665add12a76ed85f14eb78f1e
miketmaher <mike.t.t.maher@gmail.com>
2015-03-25 10:03:33 +0000

    completed JSON table of transactions, needs to be added to Admin.aspx

--- DublinBusinessSchoolCreditUnion/ErrorPage.aspx
+++ DublinBusinessSchoolCreditUnion/ErrorPage.aspx
@@ -1,5 +1,6 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Main.master" AutoEventWireup="true" CodeBehind="ErrorPage.aspx.cs" Inherits="DublinBusinessSchoolCreditUnion.ErrorPage" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+    <script src="Scripts/JSONtransactions.js"></script>
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
     <div class="row">
@@ -11,8 +12,7 @@
                 <p>The webpage you requested is not available at this time, please Log in to access the requested features</p>
             </div><!--/panel-body-->
             <div class="panel-footer">
-                <script src="Scripts/JSONtransactions.js"></script>
-                <div id="jsonTest"></div>
+                
             </div><!--/panel-footer-->
         </div><!--/panel-->
     </div><!--/row-->
--- DublinBusinessSchoolCreditUnion/Scripts/JSONtransactions.js
+++ DublinBusinessSchoolCreditUnion/Scripts/JSONtransactions.js
@@ -3,12 +3,42 @@
     $.ajax({
         url: url,
         type: "GET",
-        dataType: "json",
         success: function (data) {
             var returnData = data;
-            //BuildTable(returnData);
             console.log("Ajax Call success.");
             console.log(returnData);
+
+            var numOfTransactions = returnData.length;
+            console.log(numOfTransactions);
+            var htmlTable = [];
+            htmlTable.push("<table class='table'>");
+            htmlTable.push("<tr><th>Account Number</th><th>Destination Account</th>");
+            htmlTable.push("<th>Amount</th><th>Date &amp; Time</th>");
+            htmlTable.push("<th>Description</th><th>Type</th>");
+            htmlTable.push("</tr>");
+
+            for (i = 0; i < numOfTransactions; i++) {
+                console.log("Success: " + returnData[i].DestinationAccountNumber);
+                htmlTable.push("<tr>");
+
+                var date = new Date(parseInt(returnData[i].TransactionDateTime.substr(6)))
+                var formatted = date.getFullYear() + "-" +
+                      ("0" + (date.getMonth() + 1)).slice(-2) + "-" +
+                      ("0" + date.getDate()).slice(-2) + " " + date.getHours() + ":" +
+                      date.getMinutes();
+
+                var amount = returnData[i].TransactionAmount / 100;
+
+                htmlTable.push("<td>" + returnData[i].TransactionAccountNumber + "</td>");
+                htmlTable.push("<td>" + returnData[i].DestinationAccountNumber + "</td>");
+                htmlTable.push("<td> &euro;" + amount + "</td>");
+                htmlTable.push("<td>" + formatted + "</td>");
+                htmlTable.push("<td>" + returnData[i].TransactionDescription + "</td>");
+                htmlTable.push("<td>" + returnData[i].TransactionType + "</td>");
+                htmlTable.push("</tr>");
+            }
+            htmlTable.push("</table>");
+            $("#test").append(htmlTable.join(" "));
             
         },
         error: function (xhr) {
@@ -17,25 +47,4 @@
         }
     });
 });
-
-function BuildTable(returnData) {
-    var htmlTable = [];
-    htmlTable.push("<table class='table'>");
-
-    for (i = 0; i < returnData.Length; i++) {
-        htmlTable.push("<tr>");
-        htmlTable.push("<td>" + returnData[i].DestinationAccountNumber + "</td>");
-        htmlTable.push("<td>" + returnData[i].TransactionAccountNumber + "</td>");
-        htmlTable.push("<td>" + returnData[i].TransactionAmount + "</td>");
-        htmlTable.push("<td>" + returnData[i].TransactionDateTime + "</td>");
-        htmlTable.push("<td>" + returnData[i].TransactionDescription + "</td>");
-        htmlTable.push("<td>" + returnData[i].TransactionID + "</td>");
-        htmlTable.push("<td>" + returnData[i].TransactionReference + "</td>");
-        htmlTable.push("<td>" + returnData[i].TransactionType + "</td>");
-        htmlTable.push("</tr>");
-    }
-    htmlTable.push("</table>");
-    $("jsonTest").append(htmlTable.join(" "));
-}
-
     
